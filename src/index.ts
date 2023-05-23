7a499e920e68d0e2a2618118549e17beb22851df
Miguel Ángel Gallart <magallart58@gmail.com>
2023-05-23 15:57:35 +0200

    funcionamiento basico de subscribe y unsubscribe

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,23 @@ const observer: Observer<any> = {
   error: (error) => console.warn('error: ', error),
   complete: () => console.info('Completado'),
 };
+
+const intervalo$ = new Observable<number>((subscriber) => {
+  //crear un contador 1, 2, 3, 4,5
+  let count = 0;
+  const interval = setInterval(() => {
+    count++;
+    subscriber.next(count);
+  }, 1000);
+
+  return () => {
+    clearInterval(interval);
+    console.log('Intérvalo destruido');
+  };
+});
+
+const subscription = intervalo$.subscribe((num) => console.log('Num: ', num));
+setTimeout(() => {
+  subscription.unsubscribe();
+  console.log('Completado timeout');
+}, 3000);
