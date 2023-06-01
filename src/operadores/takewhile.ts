b608c682d209ef2270a53021bf7a4980b22adea2
Miguel Ángel Gallart <magallart58@gmail.com>
2023-06-01 16:36:25 +0200

    takeWhile

---
+++ src/operadores/takewhile.ts
@@ -0,0 +1,13 @@
+import { fromEvent, map, takeWhile } from 'rxjs';
+
+const click$ = fromEvent<PointerEvent>(document, 'click');
+
+click$
+  .pipe(
+    map(({ x, y }) => ({ x, y })),
+    takeWhile(({ y }) => y <= 150, true)
+  )
+  .subscribe({
+    next: (val) => console.log('next: ', val),
+    complete: () => console.log('Complete'),
+  });
