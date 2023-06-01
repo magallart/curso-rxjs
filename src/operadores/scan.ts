3f1ddce1be950cf75be826493883ac6bf808fe81
Miguel Ángel Gallart <magallart58@gmail.com>
2023-06-01 13:08:40 +0200

    scan

---
+++ src/operadores/scan.ts
@@ -0,0 +1,17 @@
+import { from, map, reduce, scan } from 'rxjs';
+
+const numeros = [1, 2, 3, 4, 5];
+
+// const totalAcumulador = (acc, curr) => {
+//   return acc + curr;
+// };
+
+const totalAcumulador = (acc, curr) => acc + curr;
+
+// Reduce
+
+from(numeros).pipe(reduce(totalAcumulador, 0)).subscribe(console.log);
+
+// Scan
+
+from(numeros).pipe(scan(totalAcumulador, 0)).subscribe(console.log);
