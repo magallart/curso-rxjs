4422aeb6caaf2fd910296464eb6df07c74b2397e
Miguel Ángel Gallart <magallart58@gmail.com>
2023-06-02 13:06:47 +0200

    distinct

---
+++ src/operadores/distinct.ts
@@ -0,0 +1,23 @@
+import { distinct, from, of } from 'rxjs';
+
+const numeros$ = of(1, 1, 1, 3, 3, 2, 2, 4, 4, 5, 3, 1);
+
+numeros$.pipe(distinct()).subscribe(console.log);
+
+interface Personaje {
+  nombre: string;
+}
+
+const personajes: Personaje[] = [
+  { nombre: 'Megaman' },
+  { nombre: 'X' },
+  { nombre: 'Zero' },
+  { nombre: 'Dr. Willy' },
+  { nombre: 'X' },
+  { nombre: 'Megaman' },
+  { nombre: 'Zero' },
+];
+
+from(personajes)
+  .pipe(distinct((p) => p.nombre))
+  .subscribe(console.log);
