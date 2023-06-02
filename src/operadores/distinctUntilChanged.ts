9fdd9d765f39ac2caddf37ec5c3b1f60aee337f4
Miguel Ángel Gallart <magallart58@gmail.com>
2023-06-02 13:45:33 +0200

    distinctUntilChanged

---
+++ src/operadores/distinctUntilChanged.ts
@@ -0,0 +1,23 @@
+import { distinctUntilChanged, from, of } from 'rxjs';
+
+const numeros$ = of(1, 1, 1, 3, 3, 2, 2, 4, 4, 5, 3, 1);
+
+numeros$.pipe(distinctUntilChanged()).subscribe(console.log);
+
+interface Personaje {
+  nombre: string;
+}
+
+const personajes: Personaje[] = [
+  { nombre: 'Megaman' },
+  { nombre: 'Megaman' },
+  { nombre: 'Zero' },
+  { nombre: 'Dr. Willy' },
+  { nombre: 'X' },
+  { nombre: 'X' },
+  { nombre: 'Zero' },
+];
+
+from(personajes)
+  .pipe(distinctUntilChanged((ant, act) => ant.nombre !== act.nombre))
+  .subscribe(console.log);
