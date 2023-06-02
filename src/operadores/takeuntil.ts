f9bfa9fd4b9387532a996fcacb876dab6c47d0fd
Miguel Ángel Gallart <magallart58@gmail.com>
2023-06-02 12:13:46 +0200

    takeuntil

---
+++ src/operadores/takeuntil.ts
@@ -0,0 +1,14 @@
+import { fromEvent, interval, takeUntil } from 'rxjs';
+
+const boton = document.createElement('button');
+boton.innerHTML = 'Detener timer';
+
+document.querySelector('body').append(boton);
+
+const counter$ = interval(1000);
+const clickBtn$ = fromEvent(boton, 'click');
+
+counter$.pipe(takeUntil(clickBtn$)).subscribe({
+  next: (val) => console.log('next: ', val),
+  complete: () => console.log('Complete'),
+});
