b2b9561efb1a497b5d49ed44546d3ddf16aa2c84
Miguel Ángel Gallart <magallart58@gmail.com>
2023-06-22 15:21:22 +0200

    mergeMap

---
+++ src/transformación/mergeMap.ts
@@ -0,0 +1,24 @@
+import { fromEvent, interval, map, mergeMap, of, take, takeUntil } from 'rxjs';
+
+const letras$ = of('a', 'b', 'c');
+
+letras$.pipe(
+  mergeMap((letra) =>
+    interval(1000).pipe(
+      map((i) => letra + i),
+      take(3)
+    )
+  )
+);
+// .subscribe({
+//   next: (val) => console.log('next: ', val),
+//   complete: () => console.log('Complete'),
+// });
+
+const mousedown$ = fromEvent(document, 'mousedown');
+const mouseup$ = fromEvent(document, 'mouseup');
+const interval$ = interval();
+
+mousedown$
+  .pipe(mergeMap(() => interval$.pipe(takeUntil(mouseup$))))
+  .subscribe(console.log);
