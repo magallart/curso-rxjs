415739397609822a5aab4631c6f340c6ebbb1be4
Miguel Ángel Gallart <magallart58@gmail.com>
2023-06-22 18:45:34 +0200

    ejecutar codigo en index

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const body = document.querySelector('body');
+const h2 = document.createElement('h2');
+h2.innerHTML = 'Introducir el código de los archivos aquí en el index.ts';
+
+body.append(h2);
